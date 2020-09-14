d655bad93531d91d09a28e3ed5eb7c652f98d530
David <thedavidomole@gmail.com>
2020-09-14 10:40:26 +0100

    Complete Preloader

--- js/index.js
+++ js/index.js
@@ -1,4 +1,10 @@
 window.onload = function () {
   //hide the preloader
-  document.querySelector(".preloader").style.display = "none";
+  // document.querySelector(".preloader").style.display = "none";
+  gsap.to(".preloader", 0.5, {
+    opacity: "0",
+    pointerEvents: "none",
+    delay: 3,
+    y: -50,
+  });
 };
