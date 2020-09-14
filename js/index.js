7c853fd841ff2170b86bc0f063dbb5e62ba520fe
David <thedavidomole@gmail.com>
2020-09-14 20:07:37 +0100

    Test on hd screens

--- js/index.js
+++ js/index.js
@@ -1,10 +1,14 @@
 window.onload = function () {
   //hide the preloader
   // document.querySelector(".preloader").style.display = "none";
-  gsap.to(".preloader", 0.5, {
-    opacity: "0",
-    pointerEvents: "none",
-    delay: 3,
-    y: -50,
-  });
+  const tl = new TimelineMax({ delay: 3 })
+    .to(".logo-splash", 0.4, {
+      opacity: 0,
+      delay: 0.4,
+    })
+    .to(".preloader", 0.4, {
+      //opacity: "0",
+      pointerEvents: "none",
+      height: 0,
+    });
 };
