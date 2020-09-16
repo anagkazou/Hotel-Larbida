8937965298cac01135e2a33928bc22e7f32454aa
David <thedavidomole@gmail.com>
2020-09-16 17:12:27 +0100

    Add page animations

--- js/index.js
+++ js/index.js
@@ -12,3 +12,67 @@ window.onload = function () {
       height: 0,
     });
 };
+
+// gsap.fromTo(
+//   ".anim",
+//   0.5,
+//   {
+//     scrollTrigger: {
+//       trigger: ".anim",
+//       toggleActions: "restart none none none",
+//     },
+//   },
+//   {
+//     y: "-20",
+//     opacity: 0,
+//   },
+//   {
+//     y: 0,
+//     opacity: 1,
+//   }
+// );
+
+let firstAccordionElements = document.querySelectorAll(".first-acc__el");
+let secondAccordionElements = document.querySelectorAll(".second-acc__el");
+let tl1 = new TimelineMax({
+  scrollTrigger: {
+    trigger: ".accordion",
+    start: "5% 80%",
+    toggle: "restart none none none",
+  },
+  paused: true,
+});
+
+let tl2 = new TimelineMax({
+  scrollTrigger: {
+    trigger: ".accordion-two",
+    start: "4% 80%",
+    toggle: "restart none none none",
+  },
+  paused: true,
+});
+
+firstAccordionElements.forEach((element) => {
+  tl1.from(element, {
+    y: 100,
+    opacity: 0,
+    duration: 0.4,
+  });
+});
+
+secondAccordionElements.forEach((element) => {
+  tl2.from(element, {
+    y: 100,
+    opacity: 0,
+    duration: 0.4,
+  });
+});
+
+const bookingTl = new TimelineMax({
+  scrollTrigger: ".section-booking",
+  toggle: "restart none none none",
+  markers: true,
+}).from(".section-booking", {
+  y: 80,
+  opacity: 0,
+});
